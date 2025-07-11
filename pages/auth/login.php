8ca639f7f5b54e02eadafef38a0f1afbba9ca1bf
purityko <purityongeri09@gmail.com>
2025-07-11 09:27:56 -0500

    auth

--- pages/auth/login.php
+++ pages/auth/login.php
@@ -93,6 +93,7 @@ if (!isset($_SESSION['user_id']) && isset($_COOKIE['remember_me'])) {
 <html lang="en">
 <head>
   <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
   <title>Login Page</title>
   <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
 </head>
--- pages/auth/signup.php
+++ pages/auth/signup.php
@@ -106,6 +106,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
 <html lang="en">
 <head>
   <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
   <title>Sign Up Page</title>
   <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
 </head>
